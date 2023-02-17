f9aa26296c1005adc1414ab7170dd6e3cbd7717a
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-02-17 10:17:17 +0530

    section-2 added

--- js/home.js
+++ js/home.js
@@ -1,4 +1,5 @@
 const home = `
+<!-- section-1 -->
 <div class="container">
     <div class="row align-items-center">
         <div class="col-lg-5 col-md-12">
@@ -39,4 +40,52 @@ const home = `
             </div>
         </div>
     </div>
-</div>`
+</div>
+
+<!-- section-2 -->
+<div class="container mt-5 mb-5">
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <div class="text-center mb-5">
+                <h2>Most Trusted Cryptocurrency Platform</h2>
+                    <p class="px-5">
+                        A cryptocurrency is a tradable digital asset or digital form of
+                        money, built on blockchain technology that only exists online.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="card mb-5 mb-lg-0">
+                    <h4>Best Trading Platform</h4>
+                    <p>
+                        Cryptocurrency does not exist in physical form like paper money
+                        and is typically not issued by a central authority.
+                    </p>
+                    <a href="#">Explore More <i class="fa fa-arrow-right"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="card mb-5 mb-lg-0">
+                    <h4>Transparent Pricing</h4>
+                    <p>
+                        Cryptocurrency does not exist in physical form like paper money
+                        and is typically not issued by a central authority.
+                    </p>
+                    <a href="#">Explore More <i class="fa fa-arrow-right"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="card mb-5 mb-lg-0">
+                    <h4>Trusted Security</h4>
+                    <p>
+                        Cryptocurrency does not exist in physical form like paper money
+                        and is typically not issued by a central authority.
+                    </p>
+                    <a href="#">Explore More <i class="fa fa-arrow-right"></i></a>
+                </div>
+            </div>
+    </div>
+</div>           
+`
